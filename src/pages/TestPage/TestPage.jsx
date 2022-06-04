4eb46bed734baed89c6d4bddd815f5d7a35c669c
Roman Havryliuk <gavru4r@gmail.com>
2022-06-04 15:47:23 +0300

    create question list

--- src/pages/TestPage/TestPage.jsx
+++ src/pages/TestPage/TestPage.jsx
@@ -1,20 +1,43 @@
-// import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import s from "./TestPage.module.scss";
 
 const TestPage = () => {
-  //   let { questions } = useParams();
-
-  //   const test = questions ? "Testing theory" : "QA technical training";
-
-  //   console.log(test);
   return (
-    <section>
-      <p>Testing theory</p>
-      <button>Finish test</button>
-
+    <section className={s.test}>
       <div className={s.wrapper}>
-        {/* <p>{`Question ${questions} / 12`}</p> */}
-        <p></p>
+        <p className={s.heading}>{`[${"Testing theory"}]`}</p>
+        <Link className={s.finish__btn} to={"/"}>
+          Finish test
+        </Link>
+      </div>
+
+      <div className={s.question__wrapper}>
+        <p className={s.question__current}>{`Question ${"5"} / 12`}</p>
+        <p className={s.question}>What is regression testing?</p>
+        <span className={s.line}></span>
+
+        <ul className={s.question__list}>
+          {/* ...map(el=> {
+          <li></li>
+        }) */}
+
+          <li className={s.question__item}>
+            <label className={s.question__itemLabel}>
+              <input type="checkbox"></input>
+              <span className={s.question__text}>
+                This is testing of the main functionality of the application
+              </span>
+            </label>
+          </li>
+          <li className={s.question__item}>
+            <label className={s.question__itemLabel}>
+              <input type="checkbox"></input>
+              <span className={s.question__text}>
+                This is testing of the main functionality of the application
+              </span>
+            </label>
+          </li>
+        </ul>
       </div>
     </section>
   );
