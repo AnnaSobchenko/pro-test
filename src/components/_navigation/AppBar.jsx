135e3aacfb32b4697eaca796266fb14410ad61bc
Roman Zeno <zeno385@gmail.com>
2022-06-06 17:32:23 +0300

    header create

--- src/components/_navigation/AppBar.jsx
+++ src/components/_navigation/AppBar.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
 import Icons from "../../images/symbol-defs.svg";
 import s from "./AppBar.module.scss";
+import MediaQuery from "react-responsive";
+
 
 const AppBar = () => {
   return (
     <>
-      <header className={s.header}>
-        <>
+      <header className={`${s.header} ${s.container}`}>
+        <div className={s.logo}>
           <NavLink to="/">
             <svg className={s.navIcon} width="129px" height="28px">
               <use xlinkHref={`${Icons}#icon-logo`} />
             </svg>
           </NavLink>
-        </>
+        </div>
+        <div className={s.header_navLink}>
         <NavLink to="/" className={s.link} >
           Home
         </NavLink>
@@ -23,35 +26,49 @@ const AppBar = () => {
         <NavLink to="contacts" className={s.link} activeClassName={s.activLink}>
           Contacts
         </NavLink>
+        </div>
+     
+
+
         <>
           
 
-            <div className={s.letter_wrapper}>
-              <div  className={s.circle_wrapper}>
+            <div className={s.flex}>
+              <div className={s.name_wrapper}>
+              <div  className={s.letter_wrapper}>
               <span className={s.firs_letter}>{/*firsLetter*/ "Q"}</span>
               </div>
             <span className={s.name}>{"Qwerty"}</span>
             </div>
+            </div>
           
-
+            <MediaQuery maxWidth={766}>
           <NavLink to="*">
-            <div>
+            <div className={s.navIconMenu_wrapper}>
             <svg className={s.navIconMenu} width="20px" height="20px">
               <use xlinkHref={`${Icons}#icon-menu`} />
             </svg>
             </div>
           </NavLink>
-          
+          </MediaQuery>
+
+          <MediaQuery minWidth={768}>
           <NavLink to="*">
-            <svg className={s.navIconMenu} width="16px" height="16px">
+          <div className={s.navIconMenu_wrapper}>
+            <svg className={s.navIcon_signOut} width="16px" height="16px">
               <use xlinkHref={`${Icons}#icon-sign-out`} />
             </svg>
+            </div>
           </NavLink>
+      </MediaQuery>
+
+
+      
         </>
       </header>
-        <div className={s.header_bottom_line}></div>
       <Outlet />
-    </>
+      <div className={s.header_bottom_line}></div>
+    </> 
   );
 };
 
