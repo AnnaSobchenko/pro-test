4fd7523fa78cd05c742563294173147f18989f5f
Ivan-Loboda <Ivan.Loboda.1996@gmail.com>
2022-06-09 20:47:49 +0300

    initial comit

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,37 @@
+import {Component, useEffect} from "react";
+import { createPortal } from "react-dom";
+
+// import styles from "./modal.module.css";
+
+const modalRoot = document.getElementById("modal-root")
+
+
+const Modal = ({handleClose, children}) => {
+
+    useEffect(()=> {
+        document.addEventListener("keydown", close);
+
+        return () => document.removeEventListener("keydown", close);
+    }, []);
+
+    const close = (e)=> {
+        if(e.code === "Escape") {
+            return handleClose();
+        }
+        if(e.target === e.currentTarget) {
+            handleClose()
+        }
+    }
+
+    return createPortal((
+        <div onClick={close} className={styles.overlay}>
+            <div className={styles.content}>
+                <span onClick={close} className={styles.close}>X</span>
+                {children}
+            </div>
+        </div>
+    ), modalRoot)
+}
+};
+
+export default Modal;
