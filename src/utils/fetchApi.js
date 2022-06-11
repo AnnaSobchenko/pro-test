7c4fe554a5f769596afe62d178145991461199d6
Ivan-Loboda <Ivan.Loboda.1996@gmail.com>
2022-06-11 20:07:32 +0300

    small fix

--- src/utils/fetchApi.js
+++ src/utils/fetchApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3001";
-// axios.defaults.baseURL= "https://pro-test-rest-api.herokuapp.com"
+// axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL= "https://pro-test-rest-api.herokuapp.com"
 
 const token = {
 	set(token) {
