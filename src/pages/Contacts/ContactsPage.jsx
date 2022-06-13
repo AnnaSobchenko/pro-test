f3b824999d6e856fb3e3f299806bbe25bc49e7e5
AnnaSobchenko <annsbchnk@gmail.com>
2022-06-13 18:55:03 +0300

    bugfix container

--- src/pages/Contacts/ContactsPage.jsx
+++ src/pages/Contacts/ContactsPage.jsx
@@ -1,48 +1,55 @@
 import ContactsItem from "../../components/ContactsItem/ContactsItem";
 import Modal from "../../components/Modal/Modal";
-import ContactCard from "../../components/ContactCard"
-import s from './ContactsPage.module.scss';
+import ContactCard from "../../components/ContactCard";
+import s from "./ContactsPage.module.scss";
 // import styles from "../../components/Modal/Modal.module.scss";
-import {  useState } from "react";
-
+import { useState } from "react";
 
 const ContactsPage = () => {
-    const [modal, setModal] = useState({
-        open: false,
-        content: null  
+  const [modal, setModal] = useState({
+    open: false,
+    content: null,
+  });
+
+  const openModal = (content) => {
+    setModal({
+      open: true,
+      content,
     });
+  };
 
-    const openModal = (content) => {
-        setModal({
-            open: true,
-            content
-        })
-    }
+  const closeModal = () => {
+    setModal({
+      open: false,
+      content: null,
+    });
+  };
 
-    const closeModal = ()=> {
-        setModal({
-            open: false,
-            content: null
-        })
-    }
-
-    return ( 
+  return (
     <>
-    <div className="container">
+      <div className="container">
+        <div className={s.bg}>
+          <h2 className={s.title}>Our team</h2>
+          <div className={s.vector}></div>
+          <ul className={s.conatctList}>
+            <li className={s.liLogo}>
+              <img
+                className={s.liLogoImg}
+                src={require(`../../images/logoNodejs.png`)}
+                alt="logo"
+              />
+            </li>
+            <ContactsItem openModal={openModal} />
+          </ul>
+        </div>
+      </div>
+      {modal.open && (
+        <Modal handleClose={closeModal}>
+          <ContactCard contact={modal.content} />
+        </Modal>
+      )}
+    </>
+  );
+};
 
-    <div className={s.bg}>
-    <h2 className={s.title}>Our team</h2>
-    <div className={s.vector}></div>
-    <ul className={s.conatctList}>
-        <li className={s.liLogo}>
-            <img className={s.liLogoImg} src={require(`../../images/logoNodejs.png`)} alt="logo"  />
-        </li>
-        <ContactsItem openModal={openModal}/>
-    </ul>
-    </div> 
-    </div>
-    {modal.open && <Modal handleClose={closeModal}><ContactCard contact={modal.content} /></Modal>}
-    </>);
-}
- 
 export default ContactsPage;
